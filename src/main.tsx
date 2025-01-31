296f4c12a6a753d172c48778d40936498077a72d
Pedro1043 <pedro14205x3@gmail.com>
2025-01-30 21:27:36 -0500

    Preparado para probar primer despliegue por vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './Menu'
+import Login from './Login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Login />
     <App />
   </StrictMode>,
 )
